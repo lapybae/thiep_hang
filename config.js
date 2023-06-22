f946509c48b343b6d2924815647c37e1e891e671
lapybae <117326050+lapybae@users.noreply.github.com>
2023-06-22 18:36:13 +0700

    Add files via upload
    creat

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hằng(^)';
+const giftUrl = 'https://www.facebook.com/Hangbui06';
+const eventName = 'Happy Birthday';
+const titleCard = 'Vui vẻ nè:))';
+const contentCard = ' Happy Birthday to Hằng! Chúc đứa em gái một ngày sinh nhật tràn ngập tiếng cười và niềm vui. Wish all the good will come to you!!Send: YOUR BROTHER';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hang.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
